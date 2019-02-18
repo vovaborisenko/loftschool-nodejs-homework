df90a7ea60f22092efc5ea28f386b85ce5d5dc30
Vova Borisenko <vovaborisenko@live.com>
2019-02-18 17:57:34 +0300

    create server

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const http = require('http');
+const port = 5000;
+const interval = 5000;
 
+const requestHandler = (request, response) => {
+  let timeIn = Date.now();
+
+  setInterval(() => {
+    console.log(Date(timeIn + interval));
+  }, interval);
+  response.end('Hello Node.js Server!');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happend', err);
+  }
+  console.log(`server is lisntening on ${port}`);
+});
